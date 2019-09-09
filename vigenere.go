15d48423dc10becbabf57be02dde2a3137f63474
Daniil <36066254+DaniilOr@users.noreply.github.com>
2019-09-09 21:46:15 +0600

    Vigenere cipher in Golang

--- vigenere.go
+++ vigenere.go
@@ -5,55 +5,56 @@ import "unicode/utf8"
 func isLetter(c rune) bool {
     return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
 }
-func Fast_Encode(a, b rune) rune {
+func Fast_Encode_Upper(a, b rune) rune {
 	return (((a - 'A') + (b - 'A')) % 26) + 'A'
 }
 
-func Fast_Decode(a, b rune) rune {
+func Fast_Decode_Upper(a, b rune) rune {
 	return (((((a - 'A') - (b - 'A')) + 26) % 26) + 'A')
 }
-func To_Upper(input string) string{
-  output := []rune{}
-	for _, v := range input {
-    if(!isLetter(v)){
-      output = append(output,v)
-    }
-		if 65 <= v && v <= 90 {
-			output = append(output, v)
-		} else if 97 <= v && v <= 122 {
-			output = append(output, v-32)
-		}
-	}
-
-	return string(output)
+func Fast_Encode_Lower(a, b rune) rune {
+	return (((a - 'a') + (b - 'a')) % 26) + 'a'
 }
 
-func encrypt_vigenere(plaintext string, shift string) string{
-  var nshift, ntext string
-  nshift, ntext = To_Upper(shift), To_Upper(plaintext)
-  var  key_string string
-  out := make([]rune, 0, len(ntext))
-  key_string = strings.Repeat(nshift, utf8.RuneCountInString(ntext))
-  for i, ch := range ntext {
+func Fast_Decode_Lower(a, b rune) rune {
+	return (((((a - 'a') - (b - 'a')) + 26) % 26) + 'a')
+}
 
-      out = append(out, Fast_Encode(ch, rune(  key_string[i])))
+
+func encrypt_vigenere(plaintext string, shift string) string{
+
+  var  key_string string
+  out := make([]rune, 0, len(plaintext))
+  key_string = strings.Repeat(shift, utf8.RuneCountInString(plaintext))
+  for i, ch := range plaintext {
+      if !isLetter(rune(plaintext[i])) {
+        out = append(out,rune(plaintext[i]))
+      } else if rune(plaintext[i])>='a' && rune(plaintext[i])<='z'{
+      out = append(out, Fast_Encode_Lower(ch, rune(key_string[i])))
+  } else{
+    out = append(out, Fast_Encode_Upper(ch, rune(key_string[i])))
   }
+}
   return string(out)
 }
 func decrypt_vigenere(ciphertext, key string) string {
-  var smsg, skey string
-	smsg, skey = To_Upper(ciphertext), To_Upper(key)
   var  key_string string
 	out := make([]rune, 0, len(ciphertext))
-  key_string = strings.Repeat(skey, utf8.RuneCountInString(smsg))
-	for i, ch := range smsg {
-		out = append(out, Fast_Decode(ch, rune(  key_string[i])))
-	}
+  key_string = strings.Repeat(key, utf8.RuneCountInString(ciphertext))
+  for i, ch := range ciphertext {
+      if !isLetter(rune(ciphertext[i])) {
+        out = append(out,rune(ciphertext[i]))
+      } else if rune(ciphertext[i])>='a' && rune(ciphertext[i])<='z'{
+      out = append(out, Fast_Decode_Lower(ch, rune(  key_string[i])))
+  } else{
+    out = append(out, Fast_Decode_Upper(ch, rune(key_string[i])))
+  }
+}
 	return string(out)
 }
 func main(){
   var str string
-  str = encrypt_vigenere("ATTACKATDAWN", "LEMON")
+  str = encrypt_vigenere("python3.5", "a")
   fmt.Println(str)
-  fmt.Println(decrypt_vigenere(str, "LEMON"))
+  fmt.Println(decrypt_vigenere(str, "a"))
 }
