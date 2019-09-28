c2f93db313055215eeeaf5e05808e2255adbb112
Daniil <36066254+DaniilOr@users.noreply.github.com>
2019-09-28 21:19:29 +0600

    Update caesar.py

--- Homework01/caesar.py
+++ Homework01/caesar.py
@@ -3,38 +3,40 @@ def encrypt_caesar(plaintext, shift):
     for char in plaintext:
         char_int = ord(char)
         if char.isalpha() and char.isupper():
-            char_int-=ord('A')
-            char_int+=shift
-            char_int%=26
+            char_int -= ord('A')
+            char_int += shift
+            char_int %= 26
             char_int += ord('A')
         if char.isalpha() and char.islower():
-            char_int-=ord('a')
-            char_int+=shift
-            char_int%=26
+            char_int -= ord('a')
+            char_int += shift
+            char_int %= 26
             char_int += ord('a')
 
-        ciphertext+=chr(char_int)
+        ciphertext += chr(char_int)
 
     return ciphertext
+
+
 def decrypt_caesar(ciphertext, shift):
     plaintext = ''
     for char in ciphertext:
         char_int = ord(char)
         if char.isalpha() and char.isupper():
-            char_int-=ord('A')
-            char_int-=shift
-            char_int%=26
+            char_int -= ord('A')
+            char_int -= shift
+            char_int %= 26
             char_int += ord('A')
         if char.isalpha() and char.islower():
-            char_int-=ord('a')
-            char_int-=shift
-            char_int%=26
+            char_int -= ord('a')
+            char_int -= shift
+            char_int %= 26
             char_int += ord('a')
 
-        plaintext+=chr(char_int)
+        plaintext += chr(char_int)
 
     return plaintext
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print(encrypt_caesar('python3.2', 1))
