7c4ce5a51d392f26aefe12c11fcda86a00a8a4ff
Daniil <36066254+DaniilOr@users.noreply.github.com>
2019-09-28 21:38:24 +0600

    Update vigenere.py

--- Homework01/vigenere.py
+++ Homework01/vigenere.py
@@ -1,44 +1,53 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    ciphertext=''
-    new_key = keyword * ((len(plaintext)//len(keyword)) +(len(plaintext)%len(keyword)))
+    ciphertext = ''
+    new_key = keyword * (len(plaintext) // len(keyword)) 
+    new_key = new_key + keyword * (len(plaintext) % len(keyword))
     for i in range(len(plaintext)):
         char = plaintext[i]
         code = ord(new_key[i])
         if char.isalpha() and chr(code).islower():
-            code = ord(new_key[i])-ord('a')
-            code = code + ord(char) - ord('a')*int(char.islower()) - ord('A')*int(char.isupper())
-            code%=26
-            code=code+ord('a')*int(char.islower()) + ord('A')*int(char.isupper())
+            code = ord(new_key[i]) - ord('a')
+            code = code + ord(char)
+            code -= ord('a') * int(char.islower())
+            code -= ord('A')*int(char.isupper())
+            code %= 26
+            code = code + ord('a') * int(char.islower()) 
+            code += ord('A') * int(char.isupper())
         if char.isalpha() and chr(code).isupper():
-            code = ord(new_key[i])-ord('A')
-            code =code + ord(char) - ord('a')*int(char.islower()) - ord('A')*int(char.isupper())
-            code%=26
-            code= code+ ord('a')*int(char.islower()) + ord('A')*int(char.isupper())
-        ciphertext+=chr(code)
+            code = ord(new_key[i]) - ord('A')
+            code = code + ord(char) - ord('a') * int(char.islower())
+            code -= ord('A') * int(char.isupper())
+            code %= 26
+            code = code + ord('a') * int(char.islower())
+            code += ord('A') * int(char.isupper())
+        ciphertext += chr(code)
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    plaintext=''
-    new_key = keyword * ((len(ciphertext)//len(keyword)) +(len(ciphertext)%len(keyword)) )
-    #print(len(new_key))
-    #print(len(ciphertext))
+    plaintext = ''
+    new_key = keyword * (len(ciphertext) // len(keyword))
+    new_key = new_key + keyword * (len(ciphertext) % len(keyword))
     for i in range(len(ciphertext)):
         char = ciphertext[i]
         code = ord(new_key[i])
         if char.isalpha() and chr(code).islower():
-            code = ord(new_key[i])-ord('a')
-            code = ord(char)-code  - ord('a')*int(char.islower()) - ord('A')*int(char.isupper())
-            code%=26
-            code=code+ord('a')*int(char.islower()) + ord('A')*int(char.isupper())
+            code = ord(new_key[i]) - ord('a')
+            code = ord(char) - code - ord('a') * int(char.islower())
+            code -= ord('A') * int(char.isupper())
+            code %= 26
+            code = code + ord('a') * int(char.islower())
+            code += ord('A') * int(char.isupper())
         if char.isalpha() and chr(code).isupper():
-            code = ord(new_key[i])-ord('A')
-            code =ord(char)-code  - ord('a')*int(char.islower()) - ord('A')*int(char.isupper())
-            code%=26
-            code= code+ ord('a')*int(char.islower()) + ord('A')*int(char.isupper())
+            code = ord(new_key[i]) - ord('A')
+            code = ord(char) - code - ord('a') * int(char.islower()) 
+            code -= ord('A') * int(char.isupper())
+            code %= 26
+            code = code + ord('a') * int(char.islower())
+            code += ord('A') * int(char.isupper())
 
-        plaintext+=chr(code)
+        plaintext += chr(code)
     return plaintext
-if __name__=='__main__':
+if __name__ == '__main__':
     print(encrypt_vigenere('python', 'gone'))
     print(decrypt_vigenere(encrypt_vigenere('python', 'gone'), "gone"))
